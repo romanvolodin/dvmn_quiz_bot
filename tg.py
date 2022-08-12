7b499ee0e46e297fd9e486bf4a42882897d185b4
Roman Volodin <roman.volodin@gmail.com>
2022-08-12 03:20:17 +0300

    Setup ConversationHandler

--- tg.py
+++ tg.py
@@ -2,8 +2,15 @@ import logging
 
 import redis
 from environs import Env
-from telegram import ReplyKeyboardMarkup
-from telegram.ext import CallbackQueryHandler, CommandHandler, Updater
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram.ext import (
+    CallbackQueryHandler,
+    CommandHandler,
+    ConversationHandler,
+    Filters,
+    MessageHandler,
+    Updater,
+)
 
 
 logging.basicConfig(
@@ -13,32 +20,65 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
+USER_CHOICE, CHECK_ANSWER = range(2)
+
+keyboard = [
+    ["Новый вопрос", "Сдаться"],
+    ["Мой счет"],
+]
+reply_markup = ReplyKeyboardMarkup(keyboard)
+
 
 def start(update, context):
-    keyboard = [
-        ["Новый вопрос", "Сдаться"],
-        ["Мой счёт"],
-    ]
-    reply_markup = ReplyKeyboardMarkup(keyboard)
-
     update.message.reply_text(
         "Привет! Я бот для викторины!", reply_markup=reply_markup
     )
+    return USER_CHOICE
 
 
-def button(update, context):
-    query = update.callback_query
-    query.answer()
+def ask_question(update, context):
+    update.message.reply_text("Да? Нет?", reply_markup=reply_markup)
+    return CHECK_ANSWER
 
-    db = context.bot_data["db"]
-    db.set(query.from_user.id, query.data)
 
-    if query.data == "question":
-        query.edit_message_text(text=f"Новый вопрос")
-    if query.data == "give_up":
-        query.edit_message_text(text=f"Вы сдались")
-    if query.data == "score":
-        query.edit_message_text(text=f"Ваш счет: 100500")
+def check_answer(update, context):
+    answer = update.message.text
+    if answer == "Сдаться":
+        # FIXME: копипаста функции give_up
+        update.message.reply_text(
+            "Правильный ответ: Да", reply_markup=reply_markup
+        )
+        return USER_CHOICE
+    if answer == "да":
+        update.message.reply_text(
+            'Правильно! Для следующего вопроса нажмите "Новый вопрос"',
+            reply_markup=reply_markup,
+        )
+        return USER_CHOICE
+    update.message.reply_text(
+        "Не верно, попробуйте еще раз.", reply_markup=reply_markup
+    )
+    return CHECK_ANSWER
+
+
+def give_up(update, context):
+    # TODO: Выводить ответ только если есть заданный вопрос
+    update.message.reply_text(
+        "Правильный ответ: Да", reply_markup=reply_markup
+    )
+    return USER_CHOICE
+
+
+def show_user_score(update, context):
+    update.message.reply_text(
+        "Ваш счет: 100500 очков.", reply_markup=reply_markup
+    )
+    return USER_CHOICE
+
+
+def cancel(update, context):
+    update.message.reply_text("Bye!", reply_markup=ReplyKeyboardRemove())
+    return ConversationHandler.END
 
 
 def main():
@@ -51,8 +91,22 @@ def main():
 
     dispatcher = updater.dispatcher
     dispatcher.bot_data["db"] = db
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CallbackQueryHandler(button))
+    dispatcher.add_handler(
+        ConversationHandler(
+            entry_points=[CommandHandler("start", start)],
+            states={
+                USER_CHOICE: [
+                    MessageHandler(
+                        Filters.regex("Новый вопрос"), ask_question
+                    ),
+                    MessageHandler(Filters.regex("Сдаться"), give_up),
+                    MessageHandler(Filters.regex("Мой счет"), show_user_score),
+                ],
+                CHECK_ANSWER: [MessageHandler(Filters.text, check_answer)],
+            },
+            fallbacks=[CommandHandler("cancel", cancel)],
+        )
+    )
     updater.start_polling()
     updater.idle()
 
