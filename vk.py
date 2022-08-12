a2a277b943d372a9976941838234060d86fe544d
Roman Volodin <roman.volodin@gmail.com>
2022-08-12 17:21:03 +0300

    Add simple VK echo bot

---
+++ vk.py
@@ -0,0 +1,27 @@
+import random
+
+import redis
+import vk_api as vk
+from environs import Env
+from vk_api.longpoll import VkEventType, VkLongPoll
+
+
+def echo(event, vk_api):
+    vk_api.messages.send(
+        user_id=event.user_id,
+        message=f"ECHO: {event.text}",
+        random_id=random.randint(1, 1000),
+    )
+
+
+if __name__ == "__main__":
+    env = Env()
+    env.read_env()
+
+    vk_session = vk.VkApi(token=env("VK_ACCESS_KEY"))
+    vk_api = vk_session.get_api()
+    longpoll = VkLongPoll(vk_session)
+
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            echo(event, vk_api)
